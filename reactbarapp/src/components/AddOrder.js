49155e5ca78c10f9610f62fc407e83f11f1d3ee5
Marta Adell <marta.adell.noguera@gmail.com>
2020-08-11 15:19:21 +0200

    updated firebase
    now tables and menu are inside bars/examplebar

--- reactbarapp/src/components/AddOrder.js
+++ reactbarapp/src/components/AddOrder.js
@@ -23,10 +23,10 @@ export default function AddOrder() {
       <input type="submit" onClick={(()=> {
           const table = document.getElementById("table").value;
           const plate = document.getElementById("plate").value;
-          firebase.firestore().doc(`/tables/${table}`).set({
+          firebase.firestore().doc(`/bars/testbar/tables/${table}`).set({
               occupied: true //aquí cambiamos el valor de la mesa a ocupada (por si no lo está)
           }, { merge: true })
-          firebase.firestore().collection(`/tables/${table}/orderItems`).doc(plate).set({
+          firebase.firestore().collection(`/bars/testbar/tables/${table}/orderItems`).doc(plate).set({
             done: false //aquí estamos creando un documento con el id del plate obtenido del select
         }, { merge: true });
         
